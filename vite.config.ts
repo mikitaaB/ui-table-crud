d526ee9f9dd42170f9826503c9bef9d2820d86a5
Mikita Buinavets <mikita.buinavets@arua.co>
2026-03-19 13:28:24 +0300

    Fix base name in config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
 	return {
-		base: mode === "production" ? "/ui-table-task/" : "/",
+		base: mode === "production" ? "/ui-table-crud/" : "/",
 		plugins: [react()],
 	};
 });
